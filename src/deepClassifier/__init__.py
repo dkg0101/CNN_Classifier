4cbe9089e260eb70e3bf2f4157e0196757fa9edc
Dhananjay <dkgurav0101@gmail.com>
2023-08-14 17:13:09 +0530

    update constants

--- src/deepClassifier/__init__.py
+++ src/deepClassifier/__init__.py
@@ -10,13 +10,14 @@ log_filepath = os.path.join(log_dir,"running_logs.log")
 os.makedirs(log_dir,exist_ok = True)
 
 logging.basicConfig(
-    level = logging.INFO
+    level = logging.INFO,
     format = logging_str,
     handlers =[
         logging.FileHandler(log_filepath),
         logging.StreamHandler(sys.stdout)
 
-    ])
+    ]
+    )
 
 
 logger = logging.getlogger('deepClassifierLogger')
