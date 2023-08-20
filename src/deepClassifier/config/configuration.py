67a59c0c4c9d2779831082add0155bbbc6490da1
Dhananjay <dkgurav0101@gmail.com>
2023-08-20 18:38:32 +0530

    all dvc stages run successfully

--- src/deepClassifier/config/configuration.py
+++ src/deepClassifier/config/configuration.py
@@ -93,7 +93,7 @@ class ConfigurationManager:
     
     def get_validation_config(self) -> EvaluationConfig:
         eval_config = EvaluationConfig(
-            path_of_model=self.config.training.train_model_path,
+            path_of_model=self.config.training.trained_model_path,
             training_data= os.path.join(self.config.data_ingestion.unzip_dir, "PetImages"),
             params_image_size=self.params.IMAGE_SIZE,
             params_batch_size=self.params.BATCH_SIZE
